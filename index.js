721ee871d7163137d32377ce3742813aa5d1d441
Igor De Paula <principe.borodin@gmail.com>
2014-11-30 18:29:19 -0200

    Add initial variable on index.js file

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 "use strict";
 
-var qrcode = function(req, res, next){
-    req.qrcode = function(){
+var qrcode = function (req, res, next) {
+    
+    /**
+     * Return a qrcode for put qrcode image on ejs template
+     * @returns mixed
+     */
+    req.qrcode = function () {
         
+        /**
+         * Endpoint google to generate a qrcode image
+         * @type String
+         */
+        var endpoint = "http://chart.googleapis.com/chart?";
+        
+        /**
+         * Properties of qrcode image
+         * @type Array
+         */
+        var properties = [];
+        
+        properties.push({name: 'cht', value: 'qr'});
+
     }
     next();
 }
